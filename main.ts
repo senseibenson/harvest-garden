284d3b1c1aad0ec18420a0054c5a007840bbdb16
senseibenson <73623987+senseibenson@users.noreply.github.com>
2021-03-22 19:16:52 -0400

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -2,7 +2,51 @@ namespace SpriteKind {
     export const Veggie = SpriteKind.create()
     export const Sprout = SpriteKind.create()
 }
+// let intro = 'Where ever something breathes Heart beating the rise and fall Of mountains, the waves upon the sky Of seas, the terror is our ignorance, thats why it is named after our home, earth Where art is locked between Gone and Destination The destiny of some other where...'
+// game.showLongText(intro, DialogLayout.Bottom)
+function chaseSprout () {
+    if (targetSprout == null) {
+        let sprouts = sprites.allOfKind(SpriteKind.Sprout)
+        let spriteIndex = randint(0, sprouts.length - 1)
+        let targetSprout = sprouts[spriteIndex]
+        targetSprout.say("Save me", 500)
+        rabbit.follow(targetSprout)
+        
+    }
+}
+sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Sprout, function (sprite, otherSprite) {
+    info.player2.changeScoreBy(1)
+    if (info.player2.score()==5){
+        pause(100)
+    game.over(false)
+}
+    otherSprite.setKind(SpriteKind.Veggie)
+    let veggieIndex= randint (0, veggies.length-1)
+    let veggieImg=veggies[veggieIndex]
+    otherSprite.setImage(veggieImg)
+    otherSprite.say("")
+    otherSprite.follow(rabbit)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Sprout, function (sprite, otherSprite) {
+    info.player1.changeScoreBy(1)
+    if (info.player1.score()==5){
+        pause(100)
+    game.over(true)
+}
+    otherSprite.setKind(SpriteKind.Veggie)
+    let veggieIndex= randint (0, veggies.length-1)
+    let veggieImg=veggies[veggieIndex]
+    otherSprite.setImage(veggieImg)
+    otherSprite.say("")
+    otherSprite.follow(player)
+})
+let groundTile: tiles.Location = null
+let groundIndex = 0
 let sprout: Sprite = null
+let spriteIndex = 0
+let sprouts: Sprite[] = []
+let targetSprout: Sprite = null
+let rabbit: Sprite = null
 let veggies = [
 img`
     . . . . . . . . . . . . . . . . 
@@ -155,7 +199,8 @@ let player = sprites.create(img`
     . . f . f . . . f . f . . . . . 
     . . f . f . . . f . f . . . . . 
     `, SpriteKind.Player)
-let rabbit = sprites.create(img`
+    player.z=10
+rabbit = sprites.create(img`
     . . . . . . 1 . . 1 1 . . . . . 
     . . . . . 1 1 . . 1 . . . . . . 
     . . . . . 1 3 . 1 1 . . . . . . 
@@ -173,6 +218,7 @@ let rabbit = sprites.create(img`
     . . . . . . 1 . 1 . . . . . . . 
     . . . . . . 1 . 1 . . . . . . . 
     `, SpriteKind.Enemy)
+    rabbit.z=10
 scene.setBackgroundColor(13)
 tiles.setTilemap(tilemap`level`)
 let availableTiles = tiles.getTilesByType(assets.tile`tile1`)
@@ -181,19 +227,13 @@ scene.cameraFollowSprite(player)
 info.player1.setScore(0)
 info.player2.setScore(0)
 game.onUpdateInterval(500, function () {
-    if (availableTiles.length> 0 ){
-    sprout = sprites.create(sproutImg, SpriteKind.Sprout)
-    tiles.placeOnRandomTile(null, assets.tile`tile1`)
-    let groundIndex = randint (0, availableTiles.length-1)
-    let groundTile=availableTiles[groundIndex]
-    tiles.placeOnTile(sprout, groundTile)
-    availableTiles.removeAt(groundIndex)
-
-
+    if (availableTiles.length > 0) {
+        sprout = sprites.create(sproutImg, SpriteKind.Sprout)
+        tiles.placeOnRandomTile(null, assets.tile`tile1`)
+        groundIndex = randint(0, availableTiles.length - 1)
+        groundTile = availableTiles[groundIndex]
+        tiles.placeOnTile(sprout, groundTile)
+        availableTiles.removeAt(groundIndex)
+        chaseSprout()
     }
-
-})
-sprites.onOverlap(SpriteKind.Player, SpriteKind.Sprout, function(sprite: Sprite, otherSprite: Sprite) {
-    info.player1.changeScoreBy(1)
-    otherSprite.destroy(effects.ashes, 1000)
 })
